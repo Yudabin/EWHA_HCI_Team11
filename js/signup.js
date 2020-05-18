abb10dfbdde292954040f955591b8ba8b135641d
Dabin <hi1793@naver.com>
2020-05-18 17:52:19 +0900

    signup페이지

---
+++ js/signup.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc(){ /*마우스포인트 다른 곳 클릭하면 보라색 줄 없어짐*/
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove('focus');
+    }
+    
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+});
