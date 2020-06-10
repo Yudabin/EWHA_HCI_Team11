b0ffaa75fcff46823f84d52cc06bac9a545c9a83
Dabin <hi1793@naver.com>
2020-06-10 23:31:26 +0900

    로그인버튼수정

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,17 @@
-const inputs = document.querySelectorAll('.input');
+// const inputs = document.querySelectorAll('.input');
+var inputs = document.querySelectorAll(".input");
 
-function focusFunc(){
+function focusFunc() {
     let parent = this.parentNode.parentNode;
     parent.classList.add('focus');
 }
 
-function blurFunc(){ /*마우스포인트 다른 곳 클릭하면 보라색 줄 없어짐*/
+function blurFunc() { /*마우스포인트 다른 곳 클릭하면 보라색 줄 없어짐*/
     let parent = this.parentNode.parentNode;
-    if(this.value == ""){
+    if (this.value == "") {
         parent.classList.remove('focus');
     }
-    
+
 }
 
 
@@ -18,3 +19,5 @@ inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
     input.addEventListener('blur', blurFunc);
 });
+
+
